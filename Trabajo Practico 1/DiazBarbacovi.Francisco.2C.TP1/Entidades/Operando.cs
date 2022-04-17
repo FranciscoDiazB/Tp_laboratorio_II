d2c3d7124c8d9aa4f5ed3340f152277d0286d3dc
FranciscoDiazB <frandiazb97@gmail.com>
2022-04-17 16:08:59 -0300

    Trabajo Practico 1 Finalizado

---
+++ Trabajo Practico 1/DiazBarbacovi.Francisco.2C.TP1/Entidades/Operando.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public string Numero
+        {
+            set
+            {
+                numero = ValidarOperando(value);
+            }
+        }
+
+        private static double ValidarOperando(string strNumero)
+        {
+            double retorno;
+
+            if (double.TryParse(strNumero, out retorno) == false)
+            {
+                retorno = 0;
+            }
+            return retorno;
+        }
+
+        private bool EsBinario(string binario)
+        {
+            bool retorno = false;
+            int acuValidez = 0;
+
+            foreach (char cadaCaracter in binario)
+            {
+                if (cadaCaracter == '0' || cadaCaracter == '1')
+                {
+                    acuValidez++;
+                }
+            }
+
+            if (acuValidez == binario.Length)
+            {
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            string nDecimalString = "Valor Invalido";
+            int resto = 0;
+            double nDecimalDouble = 0;
+            int i = 0;
+
+            if (EsBinario(binario))
+            {
+                int aux = int.Parse(binario);
+                while (aux != 0)
+                {
+                    resto = aux % 10;
+                    aux = aux / 10;
+                    nDecimalDouble = nDecimalDouble + resto * Math.Pow(2, i);
+                    i++;
+                }
+                nDecimalString = nDecimalDouble.ToString();
+            }
+            return nDecimalString;
+        }
+
+        public string DecimalBinario(double numero)
+        {
+            return DecimalBinario(numero.ToString());
+        }
+
+        public string DecimalBinario(string numero)
+        {
+            string nBinarioString = string.Empty;
+            int numeroAuxiliar;
+            int restoAuxiliar = 0;
+            double auxNumero;
+
+            auxNumero = double.Parse(numero);
+
+            numeroAuxiliar = (int)auxNumero;
+
+            while (numeroAuxiliar > 0)
+            {
+                restoAuxiliar = numeroAuxiliar % 2;
+                nBinarioString = restoAuxiliar.ToString() + nBinarioString;
+                numeroAuxiliar = numeroAuxiliar / 2;
+            }
+            return nBinarioString;
+        }
+
+        public Operando() : this(0)
+        {
+
+        }
+
+        public Operando(double numero) : this(numero.ToString())
+        {
+
+        }
+
+        public Operando(string strNumero)
+        {
+            if (ValidarOperando(strNumero) != 0)
+            {
+                this.numero = double.Parse(strNumero);
+            }
+        }
+
+        public static double operator +(Operando num1, Operando num2)
+        {
+            return num1.numero + num2.numero;
+        }
+
+        public static double operator -(Operando num1, Operando num2)
+        {
+            return num1.numero - num2.numero;
+        }
+
+        public static double operator /(Operando num1, Operando num2)
+        {
+            double retorno;
+
+            if (num2.numero == 0)
+            {
+                retorno = double.MinValue;
+            }
+            else
+            {
+                retorno = num1.numero / num2.numero;
+            }
+            return retorno;
+        }
+
+        public static double operator *(Operando num1, Operando num2)
+        {
+            return num1.numero * num2.numero;
+        }
+    }
+}
---
+++ Trabajo Practico 1/DiazBarbacovi.Francisco.2C.TP1/MiCalculadora/FormCalculadora.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace MiCalculadora
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.cmbOperador = new System.Windows.Forms.ComboBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lstOperaciones = new System.Windows.Forms.ListBox();
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnConvertirABinario = new System.Windows.Forms.Button();
+            this.btnConvertirADecimal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumero1.Location = new System.Drawing.Point(37, 104);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(157, 41);
+            this.txtNumero1.TabIndex = 0;
+            // 
+            // cmbOperador
+            // 
+            this.cmbOperador.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperador.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cmbOperador.FormattingEnabled = true;
+            this.cmbOperador.Items.AddRange(new object[] {
+            "",
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.cmbOperador.Location = new System.Drawing.Point(200, 104);
+            this.cmbOperador.Name = "cmbOperador";
+            this.cmbOperador.Size = new System.Drawing.Size(87, 39);
+            this.cmbOperador.TabIndex = 1;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNumero2.Location = new System.Drawing.Point(293, 104);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(157, 41);
+            this.txtNumero2.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(307, 66);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(21, 23);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "0";
+            // 
+            // lstOperaciones
+            // 
+            this.lstOperaciones.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstOperaciones.HorizontalScrollbar = true;
+            this.lstOperaciones.ItemHeight = 28;
+            this.lstOperaciones.Location = new System.Drawing.Point(488, 66);
+            this.lstOperaciones.Name = "lstOperaciones";
+            this.lstOperaciones.Size = new System.Drawing.Size(277, 200);
+            this.lstOperaciones.TabIndex = 4;
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(37, 151);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(123, 47);
+            this.btnOperar.TabIndex = 5;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(166, 151);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(155, 47);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(327, 151);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(123, 47);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnConvertirABinario
+            // 
+            this.btnConvertirABinario.Location = new System.Drawing.Point(37, 204);
+            this.btnConvertirABinario.Name = "btnConvertirABinario";
+            this.btnConvertirABinario.Size = new System.Drawing.Size(203, 48);
+            this.btnConvertirABinario.TabIndex = 8;
+            this.btnConvertirABinario.Text = "Convertir a Binario";
+            this.btnConvertirABinario.UseVisualStyleBackColor = true;
+            this.btnConvertirABinario.Click += new System.EventHandler(this.btnConvertirABinario_Click);
+            // 
+            // btnConvertirADecimal
+            // 
+            this.btnConvertirADecimal.Location = new System.Drawing.Point(247, 204);
+            this.btnConvertirADecimal.Name = "btnConvertirADecimal";
+            this.btnConvertirADecimal.Size = new System.Drawing.Size(203, 48);
+            this.btnConvertirADecimal.TabIndex = 9;
+            this.btnConvertirADecimal.Text = "Convertir a Decimal";
+            this.btnConvertirADecimal.UseVisualStyleBackColor = true;
+            this.btnConvertirADecimal.Click += new System.EventHandler(this.btnConvertirADecimal_Click);
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 353);
+            this.Controls.Add(this.btnConvertirADecimal);
+            this.Controls.Add(this.btnConvertirABinario);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.Controls.Add(this.lstOperaciones);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.cmbOperador);
+            this.Controls.Add(this.txtNumero1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Francisco Diaz Barbacovi 2C";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormCalculadora_FormClosing);
+            this.Load += new System.EventHandler(this.FormCalculadora_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.ComboBox cmbOperador;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ListBox lstOperaciones;
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnConvertirABinario;
+        private System.Windows.Forms.Button btnConvertirADecimal;
+    }
+}
