0c2267f926fd988508137886998e7f79c1bb7479
FranciscoDiazB <frandiazb97@gmail.com>
2022-06-19 19:36:08 -0300

    TP4 Finalizado

--- Trabajo Practico 4/SQLQueryClientes.sql
+++
@@ -1,8 +0,0 @@
-
-Delete from Clientes
-
-INSERT INTO Clientes (dni, nombre, apellido, metodoPago) VALUES ('18923900', 'Jose', 'Silvero', 'Efectivo')
-INSERT INTO Clientes (dni, nombre, apellido, metodoPago) VALUES ('29832100', 'Juana', 'Escudero', 'Transferencia')
-INSERT INTO Clientes (dni, nombre, apellido, metodoPago) VALUES ('42786985', 'Mariano', 'Perez', 'Efectivo')
-INSERT INTO Clientes (dni, nombre, apellido, metodoPago) VALUES ('37890450', 'Celeste', 'Rodriguez', 'Tarjeta de Credito')
-INSERT INTO Clientes (dni, nombre, apellido, metodoPago) VALUES ('22367890', 'Bruno', 'Barbacovi Gonzalez', 'Tarjeta de Debito')
